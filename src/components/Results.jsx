738e66dc6388d8eb1cceefa9a74e59aa65345990
Anders <andersmurphy@gmail.com>
2016-12-05 18:08:13 +0000

    refactors results into two components

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,32 +1,13 @@
 import React from 'react'
 import Winner from './Winner'
-
+import Tally from './Tally'
 
 export default class Result extends React.PureComponent {
-  getPair() {
-    return this.props.pair || []
-  }
-
-  getVotes(entry) {
-    if(this.props.tally && this.props.tally.has(entry)) {
-      return this.props.tally.get(entry)
-    }
-    return 0
-  }
-
   render() {
     return this.props.winner ?
     <Winner ref="winner" winner={this.props.winner} /> :
     <div className="results">
-      <div className="tally">
-        {this.getPair().map(entry =>
-          <div key={entry} className="entry">
-            <h1>{entry}</h1>
-            <div className="voteCount">
-              {this.getVotes(entry)}
-            </div>
-          </div>)}
-        </div>
+      <Tally {...this.props} />
         <div className="management">
           <button ref="next"
                   className="next"
