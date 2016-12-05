40a649bafc302c6b5240c472b5f1c4075e8de24d
Anders <andersmurphy@gmail.com>
2016-12-05 15:05:52 +0000

    adds next button to result page

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Winner from './Winner'
+
 
 export default class Result extends React.PureComponent {
   getPair() {
@@ -13,14 +15,26 @@ export default class Result extends React.PureComponent {
   }
 
   render() {
-    return <div className="results">
-      {this.getPair().map(entry =>
-        <div key={entry} className="entry">
-          <h1>{entry}</h1>
-          <div className="voteCount">
-            {this.getVotes(entry)}
-          </div>
-        </div>)}
+    return this.props.winner ?
+    <Winner ref="winner" winner={this.props.winner} /> :
+    <div className="results">
+      <div className="tally">
+        {this.getPair().map(entry =>
+          <div key={entry} className="entry">
+            <h1>{entry}</h1>
+            <div className="voteCount">
+              {this.getVotes(entry)}
+            </div>
+          </div>)}
+        </div>
+        <div className="management">
+          <button ref="next"
+                  className="next"
+                  onClick={this.props.next}>
+                  Next
+          </button>
+      </div>
     </div>
+
   }
 }
