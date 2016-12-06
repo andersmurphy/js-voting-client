ac62de44a67124532b5e8ee8f3405795caa8f1e0
Anders <andersmurphy@gmail.com>
2016-12-06 20:19:30 +0000

    adds vote and reset vote behaviour to reducer

--- src/index.jsx
+++ src/index.jsx
@@ -7,17 +7,15 @@ import {createStore} from 'redux'
 import reducer from './reducer'
 import {Provider} from 'react-redux'
 import {VotingContainer} from './components/Voting'
+import io from 'socket.io-client'
+
 
 const store = createStore(reducer)
-store.dispatch({
-  type: 'SET_STATE',
-  state: {
-    vote: {
-      pair: ['Sunshine', '28 Days Later'],
-      tally: {Sunshine: 2}
-    }
-  }
-})
+
+const socket = io(`${location.protocol}//${location.hostname}:8090`)
+socket.on('state', state =>
+  store.dispatch({type: 'SET_STATE', state})
+)
 
 const routes = <Route component={App}>
   <Route path="/results" component={ResultsContainer} />
