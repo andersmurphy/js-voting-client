e613d59acc3e46b7666680b297778bd4d685ea71
Anders <andersmurphy@gmail.com>
2016-12-06 21:15:52 +0000

    adds redux middleware
    Adds redux middleware (pre store action). These can be built up
    using currying.

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom'
 import {Route, Router, hashHistory} from 'react-router'
 import App from './components/App'
 import {ResultsContainer} from './components/Results'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import reducer from './reducer'
 import {Provider} from 'react-redux'
 import {VotingContainer} from './components/Voting'
 import io from 'socket.io-client'
 import {setState} from './action_creators'
+import remoteActionMiddleware from './remote_action_middleware'
 
 
-const store = createStore(reducer)
+const createStoreWithMiddleware = applyMiddleware(
+  remoteActionMiddleware
+)(createStore)
+const store = createStoreWithMiddleware(reducer)
 
 const socket = io(`${location.protocol}//${location.hostname}:8090`)
 socket.on('state', state =>
